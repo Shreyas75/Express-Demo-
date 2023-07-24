1f71fe810e5c11da1618ee78ed6e5f32f58e50aa
Shreyas Manoti <manotishreyas@gmail.com>
2023-07-24 19:46:05 +0530

    First Commit

---
+++ controllers/messages.controller.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+function getMessages(req,res) {
+    res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
+    // res.send('<ul><li>Hello Albert!</li></ul>')
+}
+
+function postMessage(req,res) {
+    console.log('Updating Messages....');
+}
+
+module.exports = {
+    getMessages,
+    postMessage
+}
+
---
+++ models/friends.model.js
@@ -0,0 +1,12 @@
+const friends = [
+    {
+        id: 0,
+        name: 'Sir Isaac Newton'
+    },
+    {
+        id: 1,
+        name: 'Sir Albert Einstein'
+    },
+];
+
+module.exports = friends;
